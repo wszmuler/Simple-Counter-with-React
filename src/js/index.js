2d3b5b6a0af83b4a02b3a160ab5c275a3ebf42cf
wszmuler <walter_szmuler@hotmail.com>
2019-10-01 00:23:06 +0000

    Adding Simple Counter

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,46 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+//import { Home } from "./component/home.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let digitone, digittwo, digitthree, digitfour, digitfive, digitsix;
+
+function SecondsCounter() {
+	return (
+		<div id="app">
+			<div class="counter">
+				<div class="clock-img">
+					<i class="fas fa-clock" />
+				</div>
+				<div>{digitone}</div>
+				<div>{digittwo}</div>
+				<div>{digitthree}</div>
+				<div>{digitfour}</div>
+				<div>{digitfive}</div>
+				<div>{digitsix}</div>
+			</div>
+		</div>
+	);
+}
+let digitcounter = 0;
+setInterval(function() {
+	digitone = Math.floor((digitcounter / 100000) % 10);
+	digittwo = Math.floor((digitcounter / 10000) % 10);
+	digitthree = Math.floor((digitcounter / 1000) % 10);
+	digitfour = Math.floor((digitcounter / 100) % 10);
+	digitfive = Math.floor((digitcounter / 10) % 10);
+	digitsix = Math.floor((digitcounter / 1) % 10);
+	digitcounter++;
+	//console.log(digitone, digittwo, digitthree, digitfour, digitfive, digitsix);
+	ReactDOM.render(
+		<SecondsCounter
+			digitone
+			digittwo
+			digitthree
+			digitfour
+			digitfive
+			digitsix
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
